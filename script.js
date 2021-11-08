0b7ca72ce100a70867ff83fbf631d393cad19e13
josiah-singleton <91489627+josiah-singleton@users.noreply.github.com>
2021-11-08 09:37:21 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+    let x;
+    let y;
+x = parseInt(prompt("enter the first number"));
+y = parseInt(prompt("enter the second number"));
+let z = x+y;
+let a = x-y;
+let b = x*y;
+let c = x/y;
+alert("the sum of given numbers is:"+z)
+alert("the diffrence of given numbers is:"+a)
+alert("the product of given numbers is:"+b)
+alert("the quotient of given numbers is:"+c)
+console.log (z)
+console.log (a)
+console.log (b)
+console.log (c)
